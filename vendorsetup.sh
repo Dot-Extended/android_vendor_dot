c7186be234eea755912f4bc9f167b611e6b4015f
Hung Phan <phandinhhungvp2001@gmail.com>
2019-05-03 17:54:49 +0000

    Dot : update OTA

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/DotOS/android_vendor_dot/dot-p/dot.devices | sed -e 's/#.*$//' | awk '{printf "dot_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/Dot-Extended/android_vendor_dot/pie/dot.devices | sed -e 's/#.*$//' | awk '{printf "dot_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done

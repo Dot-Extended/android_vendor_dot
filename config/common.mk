mirdaVG/platform_vendor_aosip

Add Lawnchair as default launcher
- CI build Lawnchair-alpha_839
- Thanks @deletescape :)

Change-Id: Ia31a36cf899a5244536a24ca963661317b2f9146
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

config/common.mk
@@ -86,6 +86,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
+# Lawnchair
+PRODUCT_PACKAGES += Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/aosip/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 

Dot-Extended/android_vendor_dot

config/common.mk
@@ -24,6 +24,12 @@ PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
     vendor/dot/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
+# Lawnchair
+PRODUCT_PACKAGES += Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/dot/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \